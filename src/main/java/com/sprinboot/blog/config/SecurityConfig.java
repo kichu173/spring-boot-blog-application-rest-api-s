5355193d45bf3430b6822f194180dda4cde84698
Kiran Kumar K <33867697+kichu173@users.noreply.github.com>
2022-02-21 22:54:18 +0530

    Singin/Signup rest api

--- src/main/java/com/sprinboot/blog/config/SecurityConfig.java
+++ src/main/java/com/sprinboot/blog/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -47,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(PasswordEncoder());
     }
 
-//    @Override
+    //    @Override
 //    @Bean
 //    // Inmemory role based authentication In controllers @PreAuthorize for access permission. 1.postman->authorization->basic auth->Headers basic 64encoder
 //    protected UserDetailsService userDetailsService() {
@@ -57,4 +59,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .encode("admin")).roles("ADMIN").build();
 //        return new InMemoryUserDetailsManager(kiran, admin);
 //    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
